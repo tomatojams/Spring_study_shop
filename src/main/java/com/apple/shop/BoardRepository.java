05a1241fe8f24b051e8f91f6956d870de68851a7
tomatojams <tomatojams@naver.com>
2024-08-17 15:55:04 +0900

    shop

--- src/main/java/com/apple/shop/BoardRepository.java
+++ src/main/java/com/apple/shop/BoardRepository.java
@@ -2,9 +2,7 @@ package com.apple.shop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by tomatojams on 24. 8. 14.
- */
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
 }
