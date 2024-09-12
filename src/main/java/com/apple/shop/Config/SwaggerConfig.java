0d1b6a21e09fddc60bdb459ee4944aa2081b8e2a
tomatojams <tomatojams@naver.com>
2024-09-12 11:26:11 +0900

    Spring swagger세팅

---
+++ src/main/java/com/apple/shop/Config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.apple.shop.Config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("Spring AI API 테스트")
+            .version("0.1")
+            .description("API 설명"));
+  }
+}
--- src/main/java/com/apple/shop/ExController.java
+++ src/main/java/com/apple/shop/ExController.java
@@ -1,22 +1,17 @@
 package com.apple.shop;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-
+import org.springframework.http.MediaType;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import java.time.Duration;
 
-/**
- * Created by tomatojams on 24. 9. 11.
- */
 @Controller
 //@RestController
 @RequiredArgsConstructor
-public class exController {
+public class ExController {
 
   // ChatClient 생성
   private final ChatClient chatClient;
@@ -27,48 +22,48 @@ public class exController {
   @GetMapping("/ex")
   String ex() {
 
-    var boy = new member("Soma", 18);
-    System.out.println(boy.getAge());
-    System.out.println(boy.incAge());
-    System.out.println(boy.setAge(200));
     String message = "즐거운 대화하자";
-//
+
     Flux<String> output = chatClient.prompt()
         .system("한국어로, 이미지를 많이 써서 즐겁게") // 시스템 메시지 설정
         .user(message)
         .stream()
         .content(); // 응답 내용 가져오기
 
-// 스트리밍된 데이터를 실시간으로 출력
+    // 스트리밍된 데이터를 실시간으로 출력
     output.subscribe(response -> System.out.print(response));
-
+//    var boy = new member("Soma", 18);
+//    System.out.println(boy.getAge());
+//    System.out.println(boy.incAge());
+//    System.out.println(boy.setAge(200));
     return "ex";
   }
 
 }
 
-class member {
-
-  public String name;
-  @Getter
-  @Setter
-  private Integer age;
-
-  public Integer incAge() {
-    this.age += 1;
-    return this.age;
-  }
-
-  public String setAge(Integer age) {
-    if (age > 100 || age < 0) {
-      return "입력값 오류" + age;
-    }
-    this.age = age;
-    return age.toString();
-  }
-
-  public member(String name, Integer age) {
-    this.name = name;
-    this.age = age;
-  }
-}
+// 연습용 일반 클래스
+//class member {
+//
+//  public String name;
+//  @Getter
+//  @Setter
+//  private Integer age;
+//
+//  public Integer incAge() {
+//    this.age += 1;
+//    return this.age;
+//  }
+//
+//  public String setAge(Integer age) {
+//    if (age > 100 || age < 0) {
+//      return "입력값 오류" + age;
+//    }
+//    this.age = age;
+//    return age.toString();
+//  }
+//
+//  public member(String name, Integer age) {
+//    this.name = name;
+//    this.age = age;
+//  }
+//}
